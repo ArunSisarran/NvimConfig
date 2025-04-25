57ad011f1f35c562691252af982602ee1329ad84
Arun Sisarran <arunsisarran@gmail.com>
2025-04-24 21:58:09 -0400

    Update colorscheme.lua

--- nvim/lua/arun/plugins/colorscheme.lua
+++ nvim/lua/arun/plugins/colorscheme.lua
@@ -1,58 +1,53 @@
 return {
-  {
-    "catppuccin/nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("catppuccin").setup({
-        flavour = "frappe", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-            light = "latte",
-            dark = "frappe", -- Changed from "mocha" to "frappe" to maintain consistency
-        },
-        transparent_background = false, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = true, -- set to true to enable terminal colors
-        dim_inactive = {
-            enabled = false, -- dims the background color of inactive window
-            shade = "dark",
-            percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        no_italic = false, -- Force no italic
-        no_bold = false, -- Force no bold
-        no_underline = false, -- Force no underline
-        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-            comments = { "italic" }, -- Change the style of comments
-            conditionals = { "italic" },
-            loops = {},
-            functions = {},
-            keywords = {},
-            strings = {},
-            variables = {},
-            numbers = {},
-            booleans = {},
-            properties = {},
-            types = {},
-            operators = {},
-            -- miscs = {}, -- Uncomment to turn off hard-coded styles
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        integrations = {
-            cmp = true,
-            gitsigns = true,
-            nvimtree = true,
-            treesitter = true,
-            notify = false,
-            mini = {
-                enabled = true,
-                indentscope_color = "",
-            },
-            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-      })
-      -- setup must be called before loading
-      vim.cmd.colorscheme "catppuccin-frappe" -- Explicitly set to catppuccin-frappe
-    end,
-  },
+  "ellisonleao/gruvbox.nvim",
+  event = "VeryLazy",
+  priority = 1000, -- Make sure this loads before other colorschemes
+  config = function()
+    -- Set background to dark mode
+    vim.o.background = "dark"
+    
+    require("gruvbox").setup({
+      -- Add Neovim terminal colors
+      terminal_colors = true,
+      
+      -- Text style options
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      
+      -- Color inversion options
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      
+      -- Contrast can be "hard", "soft" or empty string
+      contrast = "", -- Default contrast works well for dark mode
+      
+      -- You can override specific palette colors if needed
+      palette_overrides = {},
+      
+      -- You can override highlight groups
+      overrides = {
+        -- Example of customizing a highlight group:
+        -- Comment = { fg = "#928374", italic = true }
+      },
+      
+      -- Other options
+      dim_inactive = false,
+      transparent_mode = false,
+    })
+    
+    -- Apply the colorscheme
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
